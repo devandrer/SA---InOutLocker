6db0d34e32c486db6c2143e1463095ce2b27b44c
doispac <bernardo_b_garcia@estudante.sesisenai.org.br>
2025-05-07 21:36:07 -0300

    perfil,php no sidebar

--- app/partes/sidebar.php
+++ app/partes/sidebar.php
@@ -14,7 +14,7 @@
           <img src="dist/img/Logo_InOutLocker.png" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
-          <a href="./teste-perfil.php" class="d-block">User</a>
+          <a href="./perfil.php" class="d-block">User</a>
         </div>
       </div>
 
