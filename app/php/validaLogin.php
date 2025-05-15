517af7627748cec0da6f5cad4ea5b0722a770988
Gabriel Eduardo Schulze Machado <gabriel.eduardoschulze20@gmail.com>
2025-05-14 21:28:46 -0300

    Add comentarios no arquivo validaLogin.php

--- app/php/validaLogin.php
+++ app/php/validaLogin.php
@@ -1,34 +1,35 @@
 <?php
+    //Caso não haja uma sessão ativa inicia ela
     if(session_status() !== PHP_SESSION_ACTIVE){
         session_start();
     }
-
+    //Importa as funções
     include("funcoes.php");
 
+    //Variavel de controle
     $_SESSION['logado'] = 0;
 
-    $nome = stripslashes($_POST["nNome"]);
+    //Pega o email e senha do form do login
+    $email = stripslashes($_POST["nNome"]);
     $senha = stripslashes($_POST["nSenha"]);
 
-    //$_POST - Valor enviado pelo FORM através da propriedade NAME do elemento HTML 
-    //$_GET - Valor enviado pelo FORM através da URL
-    //$_SESSION - Variável criada pelo usuário no PHP
-
+    //Abre conexão com o banco
     include("conexao.php");
+    //SELECT
     $sql = "SELECT * FROM tb_usuario "
-            ." WHERE login = '$nome' "
+            ." WHERE login = '$email' "
             ." AND senha = md5('$senha');";
+    //Executa o comando SQL e armazena o resultado
     $resultLogin = mysqli_query($conn,$sql);
+    //Fecha conexão com banco
     mysqli_close($conn);
 
     //Validar se tem retorno do BD
-    if (mysqli_num_rows($resultLogin) > 0) {  
-        
-        // enviarEmail('destino@email.com.br','Mensagem de e-mail para SA','Teste SA','Eu mesmo');
+    if (mysqli_num_rows($resultLogin) > 0) {         
 
         foreach ($resultLogin as $coluna) {
                         
-            //***Verificar os dados da consulta SQL
+            //Passa os dados do usuario do banco para a sessão
             $_SESSION['idTipoUsuario'] = $coluna['id_tipo_usuario'];
             $_SESSION['logado']        = 1;
             $_SESSION['idLogin']       = $coluna['id_usuario'];
@@ -45,7 +46,7 @@
             
         }        
     }else{
-        //Acessar a tela inicial
+        //Volta para tela de login e exibe um erro
         $_SESSION["erroLogin"] = True; 
         header('location: ../');
     } 
