5b9a43182f0b29f35737ecfb269cf5f2331c6549
Gabriel Eduardo Schulze Machado <gabriel.eduardoschulze20@gmail.com>
2025-05-26 18:53:56 -0300

    Movendo funcao para o lugar correto

--- app/php/funcaoArmario.php
+++ app/php/funcaoArmario.php
@@ -154,3 +154,28 @@ function listaArmario(){
     return $lista;
 }
 
+//Função para listar todos os armarios na tela de reservas
+function listaArmarioReserva(){
+
+    include("conexao.php");
+    $sql = "SELECT * FROM tb_armario";
+                
+    $result = mysqli_query($conn,$sql);
+    mysqli_close($conn);
+    
+    $lista = '';
+
+    //Validar se tem retorno do BD
+    if (mysqli_num_rows($result) > 0) {
+        
+        
+        foreach ($result as $coluna) {
+                      
+            //***Verificar os dados da consulta SQL
+            $lista .= '<button class="btn btn-outline-primary" name="nArmario" value='.$coluna["id_armario"].'>'.$coluna["local"].'</button>';            
+                       
+        }    
+    }
+    
+    return $lista;
+}
