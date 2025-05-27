b207f8f08bc9d57933aac217feb3e2c363ac7a20
cikarl <cibele_karl@estudante.sesisenai.org.br>
2025-05-26 21:03:55 -0300

    tela portas

--- app/porta.php
+++ app/porta.php
@@ -53,7 +53,7 @@
                   </div>
                   
                   <div class="col-3" align="right">
-                    <button type="button" class="btn btn-success" data-toggle="modal" data-target="#novaPortaModal">
+                    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#novaPortaModal">
                       Nova Porta
                     </button>
                   </div>
@@ -66,7 +66,7 @@
               <!-- /.card-header -->
               <div class="card-body">
                 <table id="tabela" class="table table-bordered table-hover">
-                  <thead>
+                  <thead class="thead-dark">
                   <tr> <!-- tabela das informações principais da portas -->
                       <th>ID</th>
                       <th>Nr da Porta</th>
@@ -96,89 +96,61 @@
       <!-- /.container-fluid -->
        
 
-      <!-- modal de adicionar nova empresa -->
+      <!-- modal de adicionar nova porta -->
 
-      <div class="modal fade" id="novaEmpresaModal">
+      <div class="modal fade" id="novaPortaModal">
         <div class="modal-dialog modal-lg">
           <div class="modal-content">
             <div class="modal-header bg-success">
-              <h4 class="modal-title">Nova Empresa</h4>
+              <h4 class="modal-title">Nova Porta</h4>
               <button type="button" class="close text-white" data-dismiss="modal" aria-label="Close">
                 <span aria-hidden="true">&times;</span>
               </button>
             </div>
             <div class="modal-body">
-              <form method="POST" action="php/salvarEmpresa.php?funcao=I" enctype="multipart/form-data"><!-- faz a inserção de uma nova empresa -->              
+              <form method="POST" action="php/salvarPorta.php?funcao=I" enctype="multipart/form-data"><!-- faz a inserção de uma nova porta -->              
                 
                 <div class="row">
                   <div class="col-8">
                     <div class="form-group">
-                      <label for="iRazao">Razão Social:</label>
-                      <input type="text" class="form-control" id="iRazao" name="nRazao" maxlength="50">
+                      <label for="iNrPorta">Nr da Porta:</label>
+                      <input type="text" class="form-control" id="iNrPorta" name="nNrPorta" maxlength="50">
                     </div>
                   </div>
 
-                  <div class="col-8">
+                  <div class="col-6">
                     <div class="form-group">
-                      <label for="iCnpj">CNPJ:</label>
-                      <input type="text" class="form-control" id="iCnpj" name="nCnpj" maxlength="50">
+                      <label for="iArmario">Armário:</label>
+                      <select name="nArmario" id="iArmario" class="form-control" required>
+                        <option value="">Selecione um armário</option>
+                        <?php echo optionPorta(); ?>
+                      </select>
                     </div>
                   </div>
 
-                  <div class="col-9">
+                  <div class="col-6">
                     <div class="form-group">
-                      <label>Endereço</label>
-                      <input required name="Endereco" type="text" class="form-control">
+                      <label for="iStatus">Status:</label>
+                      <select name="nStatus" id="iStatus" class="form-control" required>
+                        <option value="D">Disponível</option>
+                        <option value="I">Indisponível</option>
+                      </select>
                     </div>
                   </div>
 
-                  <div class="col-5">
+                  <div class="col-12 mt-2">
                     <div class="form-group">
-                      <label>Cidade</label>
-                      <input required name="Cidade" type="text" class="form-control">
+                      <input type="checkbox" id="iAtivo" name="nAtivo" checked>
+                      <label for="iAtivo">Porta Ativa</label>
                     </div>
                   </div>
-
-                  <div class="col-2">
-                    <div class="form-group">
-                      <label>UF</label>
-                      <input required name="UF" type="text" class="form-control">
-                    </div>
-                  </div>
-
-                  <div class="col-3">
-                    <div class="form-group">
-                      <label>CEP</label>
-                      <input required name="CEP" type="text" class="form-control cep">
-                    </div>
-                  </div>
-
-                  <div class="col-3">
-                    <div class="form-group">
-                      <label>Número</label>
-                      <input required name="Numero" type="text" maxlength="8" class="form-control">
-                    </div>
-                  </div>
-
-                  <div class="col-5">
-                    <div class="form-group">
-                      <label>Bairro</label>
-                      <input required name="Bairro" type="text" class="form-control">
-                    </div>
-                  </div>
-               
-                  <div class="col-12">
-                    <div class="form-group">
-                      <input type="checkbox" id="iAtivo" name="nAtivo">
-                      <label for="iAtivo">Empresa Ativa</label>
-                    </div>
-                  </div>
+                </div>
 
                 </div>
 
                 <div class="modal-footer">
                   <button type="button" class="btn btn-danger" data-dismiss="modal">Fechar</button>
-                  <button type="submit" class="btn btn-success">Salvar</button>
+                  <button type="submit" name="btSalvaPorta" value="modal_salvar" class="btn btn-success">Salvar</button>
                 </div>
                 
               </form>
