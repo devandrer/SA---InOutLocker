6a7093259b001c03942a159e0cce6c69ddc565e1
Devandrer <andre_r_borges@estudante.sesisenai.org.br>
2025-06-04 20:49:12 -0300

    ajuste

--- app/armario.php
+++ app/armario.php
@@ -39,9 +39,9 @@ include('php/funcoes.php');
     <!-- Fim Sidebar -->
 
     <!-- Content Wrapper. Contains page content -->
-    <div class="content-wrapper">
+    <div class="content-wrapper bg-white">
       <!-- Content Header (Page header) -->
-      <div class="content-header">
+      <div class="content-header bg-white">
         <!-- Espaço -->
       </div>
       <!-- /.content-header -->
