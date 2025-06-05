ba45335fedab68ad5d0411e6f2f16105df187a33
doispac <bernardo_b_garcia@estudante.sesisenai.org.br>
2025-06-04 22:17:37 -0300

    relatorio

--- app/php/relatorioMovi.php
+++ app/php/relatorioMovi.php
@@ -8,6 +8,7 @@
     //Filtros de tela
     $armario= $_POST["nArmario"];
     $porta = $_POST["nPorta"];
+    $matricula = $_POST["nMatricula"];
     $tipomovi      = $_POST["nTipoMovi"];
     $periodoinicio      = $_POST["nPeriodoInicio"];
     $periodofim      = $_POST["nPeriodoFim"];
@@ -15,6 +16,7 @@
     //Campos para WHERE
     $whereArmario   = '';
     $wherePorta = '';
+    $whereMatricula ='';
     $whereTipoMovi     = '';
     $wherePeriodoInicio      = '';
     $wherePeriodoFinal     = '';
@@ -23,6 +25,7 @@
     $_SESSION['relatMovi']      = '';
     $_SESSION['relatMoviArmario'] = '';
     $_SESSION['relatMoviPorta'] = '';
+    $_SESSION['relatMoviMatricula'] = '';
     $_SESSION['relatMoviTipo']   = '';
     $_SESSION['relatMoviPeriodoInicio']   = '';
     $_SESSION['relatMoviPeriodoFim']   = '';
@@ -34,6 +37,10 @@
 
     if($porta != '') {
         $wherePorta = " AND por.referencia LIKE '%".$porta."%' ";
+    }
+
+    if($matricula != '') {
+        $whereMatricula = " AND usu.matricula LIKE '%".$matricula."%' ";
     }
 
     if($tipomovi != '0') {
@@ -52,25 +59,39 @@
 
     include("conexao.php");
     // script sql para puxar as infos do banco
-    $sql = "SELECT 
+    $sql = "SELECT
         arm.local,
-        por.referencia, 
-        movi.status, 
-        movi.movimentacao 
-        FROM tb_movimentacao AS movi 
-        INNER JOIN tb_porta AS por ON movi.id_porta = por.id_porta 
-        INNER JOIN tb_armario AS arm ON por.id_armario = arm.id_armario 
-        WHERE arm.id_empresa = ".$_SESSION["idEmpresa"]
+        por.referencia,
+        usu.matricula,
+        movi.status,
+        movi.movimentacao
+        FROM
+        tb_usuario AS usu
+        INNER JOIN tb_movimentacao AS movi
+        ON
+        usu.id_usuario = movi.id_usuario
+        INNER JOIN tb_porta AS por
+        ON
+        movi.id_porta = por.id_porta
+        INNER JOIN tb_armario AS arm
+        ON
+        por.id_armario = arm.id_armario
+        WHERE
+        arm.id_empresa = ".$_SESSION["idEmpresa"]
         .$whereArmario
         .$wherePorta
+        .$whereMatricula
         .$whereTipoMovi
         .$wherePeriodoInicio
         .$wherePeriodoFinal;
+        
             
     $result = mysqli_query($conn,$sql);
     mysqli_close($conn);
 
     $lista = '';
+
+    
 
     //Validar se tem retorno do BD
     if (mysqli_num_rows($result) > 0) {        
@@ -82,6 +103,7 @@
             '<tr>'
                 .'<td>'.$coluna["local"].'</td>'
                 .'<td>'.$coluna["referencia"].'</td>'
+                .'<td>'.$coluna["matricula"].'</td>'
                 .'<td>'.$coluna["status"].'</td>'
                 .'<td>'.$coluna["movimentacao"].'</td>'
             .'</tr>';             
@@ -93,6 +115,7 @@
     $_SESSION['relatMovi']      = $lista;
     $_SESSION['relatMoviArmario'] = $armario;
     $_SESSION['relatMoviPorta'] = $porta;
+    $_SESSION['relatMoviMatricula'] = $matricula;
     $_SESSION['relatMoviTipo']   = $tipomovi;
     $_SESSION['relatMoviPeriodo']   = $periodo;
 
