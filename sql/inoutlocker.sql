1672f02a8ea4eba0ced227f8630338d63219a9d3
Pedesalo <claus_katzer@estudante.sc.senai.br>
2025-06-05 20:59:42 -0300

    Atualizão do banco de dados final

--- sql/inoutlocker.sql
+++ sql/inoutlocker.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 22/05/2025 às 00:59
+-- Tempo de geração: 06/06/2025 às 01:57
 -- Versão do servidor: 10.4.32-MariaDB
 -- Versão do PHP: 8.2.12
 
@@ -39,9 +39,12 @@ CREATE TABLE `tb_armario` (
 --
 
 INSERT INTO `tb_armario` (`id_armario`, `local`, `flg_ativo`, `id_empresa`) VALUES
-(1, 'Biblioteca', 'S', 1),
-(2, 'Bloco C', 'S', 1),
-(3, 'Bloco B', 'S', 1);
+(1, 'Bloco A', 'S', 1),
+(2, 'Bloco B', 'S', 1),
+(3, 'Bloco C', 'S', 1),
+(5, 'Bloco A', 'S', 3),
+(6, 'Bloco B', 'S', 3),
+(7, 'Bloco C', 'S', 3);
 
 -- --------------------------------------------------------
 
@@ -67,7 +70,8 @@ CREATE TABLE `tb_empresa` (
 --
 
 INSERT INTO `tb_empresa` (`id_empresa`, `razao_social`, `cnpj`, `endereco`, `cidade`, `uf`, `cep`, `numero`, `bairro`, `flg_ativo`) VALUES
-(1, 'SERVICO NACIONAL DE APRENDIZAGEM INDUSTRIAL-SENAI', '03.776.284/0001-09', 'Rua Arno Waldemar Döhler', 'Joinville', 'SC', '89219-510', 957, 'Zona Industrial Norte', 'S');
+(1, 'SERVICO NACIONAL DE APRENDIZAGEM INDUSTRIAL-SENAI', '03.776.284/0001-09', 'Rua Arno Waldemar Döhler', 'Joinville', 'SC', '89219-510', 957, 'Zona Industrial Norte', 'S'),
+(3, 'Whirlpool S.A.', '59.105.999/0001-86', 'Rua Rouxinol', 'Joinville', 'SC', '89225-100', 1000, 'Aventureiro', 'S');
 
 -- --------------------------------------------------------
 
@@ -82,14 +86,6 @@ CREATE TABLE `tb_movimentacao` (
   `id_usuario` int(11) NOT NULL COMMENT 'FK - Número Identificador do Usuário ',
   `id_porta` int(11) NOT NULL COMMENT 'FK -  Número Identificador da Porta'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Despejando dados para a tabela `tb_movimentacao`
---
-
-INSERT INTO `tb_movimentacao` (`id_movimentacao`, `movimentacao`, `status`, `id_usuario`, `id_porta`) VALUES
-(1, '2025-05-21 19:43:04', 'Entrada', 8, 1),
-(2, '2025-05-21 19:43:06', 'Saída', 8, 1);
 
 -- --------------------------------------------------------
 
@@ -112,10 +108,64 @@ CREATE TABLE `tb_porta` (
 INSERT INTO `tb_porta` (`id_porta`, `referencia`, `status`, `flg_ativo`, `id_armario`) VALUES
 (1, 'A001', 'D', 'S', 1),
 (2, 'A002', 'D', 'S', 1),
-(3, 'B001', 'D', 'S', 1),
-(4, 'B002', 'D', 'S', 1),
-(5, 'C001', 'D', 'S', 1),
-(6, 'C002', 'D', 'S', 1);
+(3, 'C001', 'D', 'S', 3),
+(4, 'C002', 'D', 'S', 3),
+(5, 'B001', 'D', 'S', 2),
+(6, 'B002', 'D', 'S', 2),
+(7, 'A003', 'D', 'S', 1),
+(8, 'A004', 'D', 'S', 1),
+(9, 'A005', 'D', 'S', 1),
+(10, 'A006', 'D', 'S', 1),
+(11, 'A007', 'D', 'S', 1),
+(12, 'A008', 'D', 'S', 1),
+(13, 'A009', 'D', 'S', 1),
+(14, 'A010', 'D', 'S', 1),
+(15, 'B003', 'D', 'S', 2),
+(16, 'B004', 'D', 'S', 2),
+(17, 'B005', 'D', 'S', 2),
+(18, 'B006', 'D', 'S', 2),
+(19, 'B007', 'D', 'S', 2),
+(20, 'B008', 'D', 'S', 2),
+(21, 'B009', 'D', 'S', 2),
+(22, 'B010', 'D', 'S', 2),
+(23, 'C003', 'D', 'S', 3),
+(24, 'C004', 'D', 'S', 3),
+(25, 'C005', 'D', 'S', 3),
+(26, 'C006', 'D', 'S', 3),
+(27, 'C007', 'D', 'S', 3),
+(28, 'C008', 'D', 'S', 3),
+(29, 'C009', 'D', 'S', 3),
+(30, 'C010', 'D', 'S', 3),
+(31, 'A001', 'D', 'S', 5),
+(32, 'A002', 'D', 'S', 5),
+(33, 'A003', 'D', 'S', 5),
+(34, 'A004', 'D', 'S', 5),
+(35, 'A005', 'D', 'S', 5),
+(36, 'A006', 'D', 'S', 5),
+(37, 'A007', 'D', 'S', 5),
+(38, 'A008', 'D', 'S', 5),
+(39, 'A009', 'D', 'S', 5),
+(40, 'A010', 'D', 'S', 5),
+(41, 'B001', 'D', 'S', 6),
+(42, 'B002', 'D', 'S', 6),
+(43, 'B003', 'D', 'S', 6),
+(44, 'B004', 'D', 'S', 6),
+(45, 'B005', 'D', 'S', 6),
+(46, 'B006', 'D', 'S', 6),
+(47, 'B007', 'D', 'S', 6),
+(48, 'B008', 'D', 'S', 6),
+(49, 'B009', 'D', 'S', 6),
+(50, 'B010', 'D', 'S', 6),
+(51, 'C001', 'D', 'S', 7),
+(52, 'C002', 'D', 'S', 7),
+(53, 'C003', 'D', 'S', 7),
+(54, 'C004', 'D', 'S', 7),
+(55, 'C005', 'D', 'S', 7),
+(56, 'C006', 'D', 'S', 7),
+(57, 'C007', 'D', 'S', 7),
+(58, 'C008', 'D', 'S', 7),
+(59, 'C009', 'D', 'S', 7),
+(60, 'C010', 'D', 'S', 7);
 
 -- --------------------------------------------------------
 
@@ -168,9 +218,12 @@ CREATE TABLE `tb_usuario` (
 --
 
 INSERT INTO `tb_usuario` (`id_usuario`, `matricula`, `nome`, `cpf`, `cidade`, `endereco`, `numero`, `bairro`, `cep`, `uf`, `foto`, `login`, `senha`, `flg_ativo`, `id_empresa`, `id_tipo_usuario`) VALUES
-(8, 2025001, 'João', '111.222.333-44', 'Joinville', 'Rua Rouxinol', 123, 'Aventureiro', '89225-100', 'SC', 'dist/img/usuarios/03db7394c53cf29a9a1e62b30f058d9e.png', 'j@teste.com', '202cb962ac59075b964b07152d234b70', 'S', 1, 1),
-(9, 2025002, 'Gabriel', '111.222.333-44', 'Joinville', 'Rua Rouxinol', 42, 'Aventureiro', '89225-100', 'SC', 'dist/img/usuarios/03db7394c53cf29a9a1e62b30f058d9e.png', 'g@teste.com', '202cb962ac59075b964b07152d234b70', 'S', 1, 2),
-(10, 2025003, 'Mari', '111.222.333-44', 'Joinville', 'Rua Rouxinol', 1231, 'Aventureiro', '89225-100', 'SC', 'dist/img/usuarios/03db7394c53cf29a9a1e62b30f058d9e.png', '', '202cb962ac59075b964b07152d234b70', 'S', 1, 3);
+(8, 2025001, 'Administrador', '111.222.333-44', 'Joinville', 'Rua Arno Waldemar Dohler', 957, 'Zona Industrial Norte', '89219-510', 'SC', 'dist/img/usuarios/1984d81195fd84b347d2550270a5eee0.jpg', 'adm@senai.sc.br', '202cb962ac59075b964b07152d234b70', 'S', 1, 1),
+(9, 2025002, 'Administrador', '000.000.000-00', 'Joinville', 'Avenida Edgar Nelson Meister', 2588, 'Zona Industrial Norte', '89219-501', 'SC', 'dist/img/usuarios/1984d81195fd84b347d2550270a5eee0.jpg', 'adm@whirlpool.com', '202cb962ac59075b964b07152d234b70', 'S', 3, 1),
+(10, 2025003, 'Lucas Almenida', '691.467.389-67', 'Joinville', 'Rua Alexandre Humbold', 1000, 'Santo Antônio', '89218-170', 'SC', 'dist/img/usuarios/1984d81195fd84b347d2550270a5eee0.jpg', 'lucas.almenida@whirpool.com', '202cb962ac59075b964b07152d234b70', 'S', 3, 2),
+(11, 2025004, 'Andrei Borges', '240.345.279-35', 'Joinville', 'Rua Alexandre Humbold', 20, 'Santo Antônio', '89218-170', 'SC', 'dist/img/usuarios/1984d81195fd84b347d2550270a5eee0.jpg', '', 'd41d8cd98f00b204e9800998ecf8427e', 'S', 3, 3),
+(12, 2025005, 'Roger Pereira', '520.568.409-79', 'Joinville', 'Rua Rouxinol', 550, 'Aventureiro', '89225-100', 'SC', 'dist/img/usuarios/1984d81195fd84b347d2550270a5eee0.jpg', 'roger.pereira@senai.sc.br', '202cb962ac59075b964b07152d234b70', 'S', 1, 2),
+(13, 2025006, 'Jenifer Matheus', '028.039.029-74', 'Joinville', 'Rua Rouxinol', 45, 'Aventureiro', '89225-100', 'SC', 'dist/img/usuarios/1984d81195fd84b347d2550270a5eee0.jpg', '', 'd41d8cd98f00b204e9800998ecf8427e', 'S', 1, 3);
 
 --
 -- Índices para tabelas despejadas
@@ -227,25 +280,25 @@ ALTER TABLE `tb_usuario`
 -- AUTO_INCREMENT de tabela `tb_armario`
 --
 ALTER TABLE `tb_armario`
-  MODIFY `id_armario` int(11) NOT NULL AUTO_INCREMENT COMMENT 'PK - Número Identificador do Armário', AUTO_INCREMENT=4;
+  MODIFY `id_armario` int(11) NOT NULL AUTO_INCREMENT COMMENT 'PK - Número Identificador do Armário', AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT de tabela `tb_empresa`
 --
 ALTER TABLE `tb_empresa`
-  MODIFY `id_empresa` int(11) NOT NULL AUTO_INCREMENT COMMENT 'PK - Número Identificador da empresa', AUTO_INCREMENT=2;
+  MODIFY `id_empresa` int(11) NOT NULL AUTO_INCREMENT COMMENT 'PK - Número Identificador da empresa', AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT de tabela `tb_movimentacao`
 --
 ALTER TABLE `tb_movimentacao`
-  MODIFY `id_movimentacao` int(11) NOT NULL AUTO_INCREMENT COMMENT 'PK -  Número Identificador da Movimentação', AUTO_INCREMENT=3;
+  MODIFY `id_movimentacao` int(11) NOT NULL AUTO_INCREMENT COMMENT 'PK -  Número Identificador da Movimentação', AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT de tabela `tb_porta`
 --
 ALTER TABLE `tb_porta`
-  MODIFY `id_porta` int(11) NOT NULL AUTO_INCREMENT COMMENT 'PK -  Número Identificador da Porta', AUTO_INCREMENT=7;
+  MODIFY `id_porta` int(11) NOT NULL AUTO_INCREMENT COMMENT 'PK -  Número Identificador da Porta', AUTO_INCREMENT=61;
 
 --
 -- AUTO_INCREMENT de tabela `tb_tipo_usuario`
@@ -257,7 +310,7 @@ ALTER TABLE `tb_tipo_usuario`
 -- AUTO_INCREMENT de tabela `tb_usuario`
 --
 ALTER TABLE `tb_usuario`
-  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT COMMENT 'PK -  Número Identificador do Usuário', AUTO_INCREMENT=11;
+  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT COMMENT 'PK -  Número Identificador do Usuário', AUTO_INCREMENT=14;
 
 --
 -- Restrições para tabelas despejadas
